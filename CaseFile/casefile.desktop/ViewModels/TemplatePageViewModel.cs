63448b4a124c96b31539f69a4128697c22f2160a
valoucedriclaine <valoucedriclaine@gmail.com>
2026-02-22 10:19:05 -0500

    Commence le style des cartes des elements

--- CaseFile/casefile.desktop/ViewModels/TemplatePageViewModel.cs
+++ CaseFile/casefile.desktop/ViewModels/TemplatePageViewModel.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using casefile.desktop.ViewModels.Template;
 using ReactiveUI;
 
 namespace casefile.desktop.ViewModels;
@@ -6,5 +10,43 @@ public class TemplatePageViewModel : PageViewModelBase
 {
     public TemplatePageViewModel(IScreen screen) : base(screen)
     {
+        ListeTemplate = new ObservableCollection<TemplateDossierItemViewModel>(new List<TemplateDossierItemViewModel>
+        {
+            new()
+            {
+                Id = Guid.NewGuid(),
+                Nom = "Template 1",
+                Description = "Description du template 1",
+                NombreDeDossiers = 5,
+                NombreDocumentsAttendus = 20,
+                NombreDeClientsQuiUtilisentCeTemplate = 10
+            },
+            new()
+            {
+                Id = Guid.NewGuid(),
+                Nom = "Template 2",
+                Description = "Description du template 2",
+                NombreDeDossiers = 3,
+                NombreDocumentsAttendus = 15,
+                NombreDeClientsQuiUtilisentCeTemplate = 7
+            },
+            new()
+            {
+                Id = Guid.NewGuid(),
+                Nom = "Template 3",
+                Description = "Description du template 3",
+                NombreDeDossiers = 8,
+                NombreDocumentsAttendus = 30,
+                NombreDeClientsQuiUtilisentCeTemplate = 12
+            }
+        });
     }
+
+    /// <summary>
+    /// Propriété représentant une collection observable des modèles de dossiers disponibles.
+    /// Chaque élément de la liste est une instance de <see cref="TemplateDossierItemViewModel"/>,
+    /// qui contient des informations détaillées telles que le nom, la description,
+    /// le nombre de dossiers associés et d'autres métadonnées.
+    /// </summary>
+    public ObservableCollection<TemplateDossierItemViewModel> ListeTemplate { get; set; }
 }
