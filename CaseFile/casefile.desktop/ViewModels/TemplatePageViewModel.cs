bc79ddb302082f89fa00b66cb4e8cc830f71e5a6
valoucedriclaine <valoucedriclaine@gmail.com>
2026-02-24 13:36:58 -0500

    Update

--- CaseFile/casefile.desktop/ViewModels/TemplatePageViewModel.cs
+++ CaseFile/casefile.desktop/ViewModels/TemplatePageViewModel.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using casefile.desktop.ViewModels.Template;
 using ReactiveUI;
@@ -10,37 +8,9 @@ public class TemplatePageViewModel : PageViewModelBase
 {
     public TemplatePageViewModel(IScreen screen) : base(screen)
     {
-        ListeTemplate = new ObservableCollection<TemplateDossierItemViewModel>(new List<TemplateDossierItemViewModel>
-        {
-            new()
-            {
-                Id = Guid.NewGuid(),
-                Nom = "Template 1",
-                Description = "Description du template 1",
-                NombreDeDossiers = 5,
-                NombreDocumentsAttendus = 20,
-                NombreDeClientsQuiUtilisentCeTemplate = 10
-            },
-            new()
-            {
-                Id = Guid.NewGuid(),
-                Nom = "Template 2",
-                Description = "Description du template 2",
-                NombreDeDossiers = 3,
-                NombreDocumentsAttendus = 15,
-                NombreDeClientsQuiUtilisentCeTemplate = 7
-            },
-            new()
-            {
-                Id = Guid.NewGuid(),
-                Nom = "Template 3",
-                Description = "Description du template 3",
-                NombreDeDossiers = 8,
-                NombreDocumentsAttendus = 30,
-                NombreDeClientsQuiUtilisentCeTemplate = 12
-            }
-        });
     }
+
+    public bool IsListeTemplateEmpty => ListeTemplate.Count == 0;
 
     /// <summary>
     /// Propriété représentant une collection observable des modèles de dossiers disponibles.
